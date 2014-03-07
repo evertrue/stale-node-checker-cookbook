f86c5fcdf13bb4f9243edc2d866f4cc399905347
Eric Herot <eric.github@herot.com>
2014-03-07 18:44:54 -0500

    Linting cleanup

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,6 @@ default['stale-node-checker']['threshold']['days'] = 0
 default['stale-node-checker']['threshold']['hours'] = 1
 default['stale-node-checker']['threshold']['minutes'] = 0
 
-default['stale-node-checker']['max-nodes'] = 2500
+default['stale-node-checker']['max-nodes'] = 2_500
 default['stale-node-checker']['ignore'] = []
 default['stale-node-checker']['alert-email'] = nil
