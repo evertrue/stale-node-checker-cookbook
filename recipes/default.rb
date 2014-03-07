f933fb9e597945727a382112376145b5d642dab7
Eric Herot <eric.github@herot.com>
2014-03-07 18:48:49 -0500

    Make the notifier code actually work

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe 'postfix'
+include_recipe 'postfix::aliases'
+
 search_args = {
   :keys => {
     :ohai_time => ['ohai_time'],
@@ -22,29 +25,21 @@ threshold_time = Time.now.to_i -
     node['stale-node-checker']['threshold']['minutes'] * 60
   )
 
-stale_nodes = []
-
 partial_search(
     :node,
     "ohai_time:[* TO #{threshold_time}]",
     search_args
   ).first.each do |stale_node|
   unless node['stale-node-checker']['ignore'].include?stale_node['name']
-    msg = Chef::Recipe::StaleNodeChecker.check_last_run_time(node['ohai_time'].to_i)
-    stale_nodes << stale_node['name']
+
+    # Chef::Log.info "Stale node: #{stale_node['name']}"
+    # Chef::Log.info "Stale node staleness: #{msg.inspect}"
+
+    notifier "Stale node: #{stale_node['name']}" do
+      to node['stale-node-checker']['alert-email']
+      message 'The node <' + stale_node['name'] + '> has not checked in to ' \
+        "Chef recently.\n\nThe last check-in was " \
+        "#{Time.now.to_i - stale_node['ohai_time'].to_i} seconds ago."
+    end
   end
-end
-
-Chef::Log.info "Stale nodes: #{stale_nodes.join(', ')}"
-
-notifier 'Stale nodes detected' do
-  to node['stale-node-checker']['alert-email']
-  message "The following nodes have not checked in to Chef recently: \n" +
-    "\n  " +
-    stale_nodes.join("\n  ") +
-    "Threshold: " +
-    node['stale-node-checker']['threshold']['days'].to_s + " days, " +
-    node['stale-node-checker']['threshold']['hours'].to_s + " hours, " +
-    node['stale-node-checker']['threshold']['minutes'].to_s + " minutes"
-  not_if { stale_nodes.empty? }
 end
