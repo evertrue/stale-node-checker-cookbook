eb99e1f145ddf6fe7454e89171718c90ae6d39ba
Eric Herot <eric.github@herot.com>
2014-04-26 12:37:29 -0400

    Bump threshold to 90 minutes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['stale-node-checker']['threshold']['days'] = 0
 default['stale-node-checker']['threshold']['hours'] = 1
-default['stale-node-checker']['threshold']['minutes'] = 0
+default['stale-node-checker']['threshold']['minutes'] = 30
 
 default['stale-node-checker']['max-nodes'] = 2_500
 default['stale-node-checker']['ignore'] = []
