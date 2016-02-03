8e64586292632d804f47f918236ef72a85be34b3
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-02-03 16:20:21 -0500

    Bump to v2.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures stale-node-checker'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '2.0.0'
 
 depends 'notifier'
